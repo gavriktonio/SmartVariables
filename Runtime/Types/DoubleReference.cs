9e63a91219d5b3242b84c3ddd7264fe2016c65a6
Anton Gavrilov <gavriktonio@gmail.com>
2020-09-07 16:23:36 +0200

    [ADD] DoubleReference class

---
+++ Runtime/Types/DoubleReference.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+namespace SmartVariables
+{
+    [System.Serializable]
+    [CreateAssetMenu(menuName = "Variables/Double")]
+    public class DoubleReference : SmartReference<double> { }
+
+    [System.Serializable]
+    public class SmartDouble : SmartVariable<double, DoubleReference> { }
+}
